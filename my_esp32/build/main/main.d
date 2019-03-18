887f5569673965d5488353c9c9732eda9585bc1f
rita <rita@git.com>
2019-03-18 14:13:51 +0800

    components

--- my_esp32/build/main/main.d
+++ my_esp32/build/main/main.d
@@ -1,6 +1,6 @@
 main.o: /home/rita/mygit/my_esp32/main/main.c \
- /home/rita/mygit/my_esp32/main/led.h \
- /home/rita/mygit/my_esp32/main/wifi.h \
+ /home/rita/mygit/my_esp32/components/led/led.h \
+ /home/rita/mygit/my_esp32/components/wifi/wifi.h \
  /home/rita/esp32/esp-idf/components/esp32/include/esp_wifi.h \
  /home/rita/esp32/esp-idf/components/newlib/include/stdint.h \
  /home/rita/esp32/esp-idf/components/newlib/include/machine/_default_types.h \
@@ -139,7 +139,7 @@ main.o: /home/rita/mygit/my_esp32/main/main.c \
  /home/rita/esp32/esp-idf/components/freertos/include/freertos/task.h \
  /home/rita/esp32/esp-idf/components/lwip/lwip/src/include/lwip/err.h \
  /home/rita/esp32/esp-idf/components/lwip/lwip/src/include/lwip/sys.h \
- /home/rita/mygit/my_esp32/main/tcp.h \
+ /home/rita/mygit/my_esp32/components/tcp/tcp.h \
  /home/rita/esp32/esp-idf/components/lwip/lwip/src/include/lwip/sockets.h \
  /home/rita/esp32/esp-idf/components/lwip/lwip/src/include/lwip/inet.h \
  /home/rita/esp32/esp-idf/components/lwip/lwip/src/include/lwip/errno.h \
@@ -148,9 +148,9 @@ main.o: /home/rita/mygit/my_esp32/main/main.c \
  /home/rita/esp32/esp-idf/components/newlib/include/string.h \
  /home/rita/esp32/esp-idf/components/newlib/include/sys/string.h
 
-/home/rita/mygit/my_esp32/main/led.h:
+/home/rita/mygit/my_esp32/components/led/led.h:
 
-/home/rita/mygit/my_esp32/main/wifi.h:
+/home/rita/mygit/my_esp32/components/wifi/wifi.h:
 
 /home/rita/esp32/esp-idf/components/esp32/include/esp_wifi.h:
 
@@ -428,7 +428,7 @@ main.o: /home/rita/mygit/my_esp32/main/main.c \
 
 /home/rita/esp32/esp-idf/components/lwip/lwip/src/include/lwip/sys.h:
 
-/home/rita/mygit/my_esp32/main/tcp.h:
+/home/rita/mygit/my_esp32/components/tcp/tcp.h:
 
 /home/rita/esp32/esp-idf/components/lwip/lwip/src/include/lwip/sockets.h:
 
